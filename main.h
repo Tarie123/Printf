9b661e9db7e2d2191488916b78ee090fd061d0ef
Tarie123 <tarymhlang@gmail.com>
2023-11-14 13:46:18 -0800

    Final

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int binary(int num);
+int octal(int num);
+
+#endif
